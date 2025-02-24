66bfdd0aa22d3efd65937493a26b4ab4f8c0470d
lajko.gergo <lajkogege@gmail.com>
2025-02-24 10:58:41 +0100

    Teszteles előtt

--- doga/src/Main.java
+++ doga/src/Main.java
@@ -1,8 +1,6 @@
 import modell.Garazs;
 import modell.Jarmu;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Garazs garazs= new Garazs();
